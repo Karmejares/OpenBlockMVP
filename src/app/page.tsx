e5e132c08b806bf99d709023ff5fefddb3488d22
Karmejares <jamesmoncada0120@gmail.com>
2025-05-04 02:34:14 -0500

    fixed tool bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,12 @@ import Box from "@mui/material/Box";
 import MenuComponent from "./components/MenuComponent";
 import PerfilPrestamista from "./components/PerfilPrestamista/PerfilPrestamista";
 import PerfilBeneficiario from "./components/PerfilBeneficiario/PerfilBeneficiario";
-import {ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [profileType, setProfileType] = useState("Perfil Prestamista");
   const [accountBalance, setAccountBalance] = useState(1000); // Monto inicial
-  const [setActiveTab] = useState(0); // Tab activa para Depositar/Retirar
 
   const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -36,7 +35,8 @@ export default function Home() {
 
   return (
     <Box sx={{ backgroundColor: "black", height: "100vh", color: "white" }}>
-      <AppBar position="static" sx={{ backgroundColor: "black" }}>
+      {/* Fixed AppBar */}
+      <AppBar position="fixed" sx={{ backgroundColor: "black", zIndex: 1200 }}>
         <Toolbar>
           <IconButton
             edge="start"
@@ -64,16 +64,21 @@ export default function Home() {
           </button>
         </Toolbar>
       </AppBar>
-      <MenuComponent anchorEl={anchorEl} handleMenuClose={handleMenuClose} />
-      {profileType === "Perfil Prestamista" ? (
-        <PerfilPrestamista
-          accountBalance={accountBalance}
-          setAccountBalance={setAccountBalance}
-        />
-      ) : (
-        <PerfilBeneficiario />
-      )}
-      <ToastContainer/>
+
+      {/* Add padding to prevent overlap */}
+      <Box sx={{ paddingTop: "64px" }}>
+        <MenuComponent anchorEl={anchorEl} handleMenuClose={handleMenuClose} />
+        {profileType === "Perfil Prestamista" ? (
+          <PerfilPrestamista
+            accountBalance={accountBalance}
+            setAccountBalance={setAccountBalance}
+          />
+        ) : (
+          <PerfilBeneficiario />
+        )}
+      </Box>
+
+      <ToastContainer />
     </Box>
   );
 }
