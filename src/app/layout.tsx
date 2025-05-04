08727dc52d90e178143dfde694a6776a4040d9e7
Karmejares <jamesmoncada0120@gmail.com>
2025-05-04 11:56:59 -0500

    correting text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,27 @@
+"use client";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "../createEmotionCache";
+import theme from "../theme";
+
+const clientSideEmotionCache = createEmotionCache();
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
-      <body>{children}</body>
+    <html lang="en">
+      <body>
+        <CacheProvider value={clientSideEmotionCache}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </CacheProvider>
+      </body>
     </html>
   );
 }
